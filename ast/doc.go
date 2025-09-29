cbed2ffafb4b2fc593a9164539ec454601822ef3
sam <aquaticcalf@proton.me>
2025-09-29 11:10:53 +0530

    add Project struct and Track interface

---
+++ ast/doc.go
@@ -0,0 +1,2 @@
+// Package ast defines and parses the data structures from and to DSL and ffmpeg in rangastalam
+package ast
--- ast/types.go
+++ ast/types.go
@@ -1,1 +1,13 @@
 package ast
+
+type Project struct {
+	Width  int
+	Height int
+	FPS    float64
+	Tracks []Track
+}
+
+type Track interface {
+	Name() string
+	Z() int
+}
