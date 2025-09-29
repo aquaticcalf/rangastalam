0e48b67eeaca64454fd5c28756b2c0592abb88ae
sam <aquaticcalf@proton.me>
2025-09-29 12:27:34 +0530

    add Image and Audio track types

--- ast/types.go
+++ ast/types.go
@@ -16,11 +16,13 @@ type Track interface {
 }
 
 type Video struct {
-	Name  string
-	Clips []VideoNode
+	Name   string
+	Zindex int
+	Clips  []VideoNode
 }
 
 func (v Video) TrackName() string { return v.Name }
+func (v Video) Z() int            { return v.Zindex }
 
 type VideoNode struct {
 	ID       string
@@ -35,10 +37,12 @@ type VideoNode struct {
 
 type Text struct {
 	Name     string
+	Zindex   int
 	Contents []TextNode
 }
 
 func (t Text) TrackName() string { return t.Name }
+func (t Text) Z() int            { return t.Zindex }
 
 type TextNode struct {
 	ID      string
@@ -54,4 +58,42 @@ type TextStyle struct {
 	Size  float64
 	Color string
 	Align string
+}
+
+type Image struct {
+	Name   string
+	Zindex int
+	Images []ImageNode
+}
+
+func (i Image) TrackName() string { return i.Name }
+func (i Image) Z() int            { return i.Zindex }
+
+type ImageNode struct {
+	ID    string
+	Path  string
+	Start float64
+	End   float64
+	Pos   common.Vec2
+	Size  common.Size
+}
+
+type Audio struct {
+	Name   string
+	Zindex int
+	Audios []AudioNode
+}
+
+func (a Audio) TrackName() string { return a.Name }
+func (a Audio) Z() int            { return a.Zindex }
+
+type AudioNode struct {
+	ID       string
+	Path     string
+	SrcStart float64
+	SrcEnd   float64
+	Start    float64
+	End      float64
+	Volume   float64
+	Loop     bool
 }
