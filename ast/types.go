b134631880682cac278d78ae9d9affbe94b3689e
sam <aquaticcalf@proton.me>
2025-09-29 11:51:54 +0530

    add Video and Text track types

--- ast/types.go
+++ ast/types.go
@@ -1,13 +1,57 @@
 package ast
 
+import (
+	"github.com/aquaticcalf/rangastalam/common"
+)
+
 type Project struct {
-	Width  int
-	Height int
+	Size   common.Size
 	FPS    float64
 	Tracks []Track
 }
 
 type Track interface {
-	Name() string
+	TrackName() string
 	Z() int
+}
+
+type Video struct {
+	Name  string
+	Clips []VideoNode
+}
+
+func (v Video) TrackName() string { return v.Name }
+
+type VideoNode struct {
+	ID       string
+	Path     string
+	SrcStart float64
+	SrcEnd   float64
+	Start    float64
+	End      float64
+	Pos      common.Vec2
+	Size     common.Size
+}
+
+type Text struct {
+	Name     string
+	Contents []TextNode
+}
+
+func (t Text) TrackName() string { return t.Name }
+
+type TextNode struct {
+	ID      string
+	Content string
+	Start   float64
+	End     float64
+	Pos     common.Vec2
+	Style   TextStyle
+}
+
+type TextStyle struct {
+	Font  string
+	Size  float64
+	Color string
+	Align string
 }
